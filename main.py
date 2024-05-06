9e89f47d1acd9741c6423e550f255771b6be23d1
Nisansala Suranjani <62841582+nisansalaperera@users.noreply.github.com>
2024-05-06 11:14:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+input_str=input("Enter string:")
+
+try:
+    index1=int(input("Enter 1st Index:"))
+except ValueError:
+    print("Please input an integer for the first index")
+
+try:
+    index2=int(input("Enter 2nd Index;"))
+except ValueError:
+    print("Please input an integer for the second index")
+
+def same_chars(str,i1,i2):
+    
+    try:
+        if str[i1] == str[i2]:
+            return True
+        else:
+            return False
+    except IndexError:
+        return False
+
+print (same_chars(input_str,index1,index2))
